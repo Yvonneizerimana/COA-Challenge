37ad4eaba87c3bf33fa57565a3a081adb1dbe455
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-06-07 19:06:01 +0100

    i explained arrayMap codes that i've written

--- Challenges/arrayMap.js
+++ Challenges/arrayMap.js
@@ -1,10 +1,13 @@
 let contigousSubArrayWithinTheSum=(array,target)=>{
+    //initialise the current sum of the elements in the subarray
         let currentSum=0;
+        //initialise the starting index of the subarray
         let start=0;
-
+//loop through each the element in the subarray
         for (let i=0; i<array.length; i++){
+            //add current element to the currentSum
             currentSum+=array[i];
-        
+        // If the currentSum exceeds the target, shift the starting index of the subarray until the currentSum is less than or equal to the target.
             while(currentSum>target && start<=i){
                 currentSum-=array[start];
                 start++;
@@ -17,7 +20,7 @@ let contigousSubArrayWithinTheSum=(array,target)=>{
         }
         return false
 }
-
+//here there is example usage:
 const array=[4, 2, 7, 1, 9, 5];
 const target=17;
 console.log(contigousSubArrayWithinTheSum(array,target));
